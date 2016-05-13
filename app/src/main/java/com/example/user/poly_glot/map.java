c9282176bc36fbf593479e203274c4498e99d2f5
djsharox <djsharox@gmail.com>
2016-05-13 09:53:07 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/user/poly_glot/map.java
@@ -0,0 +1,93 @@
+package com.example.user.poly_glot;
+
+
+import android.app.Dialog;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.GoogleMap;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    GoogleMap mMap;
+    private static final int ERROR_DIALOG_REQUEST =  9001;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        if(servicesOK()){
+            setContentView(R.layout.activity_map);
+            Toast.makeText(this,"cant connect",Toast.LENGTH_SHORT).show();
+        }
+        else {
+            setContentView(R.layout.activity_main);
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean servicesOK(){
+
+        int isAvailable = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+
+        if (isAvailable == ConnectionResult.SUCCESS){
+            return true;}
+
+
+        else if(GooglePlayServicesUtil.isUserRecoverableError(isAvailable)){
+
+            Dialog dialog =
+                    GooglePlayServicesUtil.getErrorDialog(isAvailable,this,ERROR_DIALOG_REQUEST);
+            dialog.show();}
+
+
+        else{
+
+
+            Toast.makeText(this,"cant connect",Toast.LENGTH_SHORT).show();
+        }
+
+
+
+
+        return false;
+
+
+
+    }
+
+
+
+}
